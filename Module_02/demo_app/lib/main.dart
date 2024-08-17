5406b056cfe312d46fcf0d51523fb830177add26
Ramasheesh <rch231198@gmail.com>
2024-08-17 15:11:33 +0530

    button added

--- Module_02/demo_app/lib/main.dart
+++ Module_02/demo_app/lib/main.dart
@@ -10,23 +10,120 @@ class MyApp extends StatelessWidget {
       title: 'Hello Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'Home page '),  
+      home: MyHomePage(title: 'Home page '),
     );
   }
 }
+
 class MyHomePage extends StatelessWidget {
-  MyHomePage({Key ? key , required this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(this.title)
-      ),
-      body: const Center(
-        child: Text('Hello Flutter'),
+      appBar: AppBar(title: Text(this.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: const Text('Go to second page'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdPage(title: 'Page of button',)),
+                );
+              },
+              child: const Text('Go to All Button Page'),
+            ),
+          ],
+        ),
       ),
     );
+
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  ThirdPage({Key? key, required this.title}) : super(key: key);
+  final String title;  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+      title: 'Button Types',
+      home: const Scaffold(
+        body: ButtonTypesExample(),
+      ),
+    );
+  }
+}
+
+class ButtonTypesExample extends StatelessWidget {
+  const ButtonTypesExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Spacer(),
+          ButtonTypesGroup(enabled: true),
+          ButtonTypesGroup(enabled: false),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonTypesGroup extends StatelessWidget {
+  const ButtonTypesGroup({super.key, required this.enabled});
+
+  final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    final VoidCallback? onPressed = enabled ? () {} : null;
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          ElevatedButton(onPressed: onPressed, child: const Text('Elevated')),
+          FilledButton(onPressed: onPressed, child: const Text('Filled')),
+          FilledButton.tonal(
+              onPressed: onPressed, child: const Text('Filled Tonal')),
+          OutlinedButton(onPressed: onPressed, child: const Text('Outlined')),
+          TextButton(onPressed: onPressed, child: const Text('Text')),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Second Page'),
+        ),
+        body: Text('this is second Page'));
   }
 }
